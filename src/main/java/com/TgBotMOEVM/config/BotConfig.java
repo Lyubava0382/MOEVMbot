7dbf72731388ba76cc08f4e5f48058db043007b8
Lyubava0382 <lubovkrivencova@gmail.com>
2024-04-05 00:55:12 +0300

    Demo bot version 2

--- src/main/java/com/TgBotMOEVM/config/BotConfig.java
+++ src/main/java/com/TgBotMOEVM/config/BotConfig.java
@@ -1,13 +1,15 @@
 package com.TgBotMOEVM.config;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@Data
-@PropertySource("application.properties")
+@Setter
+@Getter
 public class BotConfig {
     @Value("${bot.name}")
     private String botName;
