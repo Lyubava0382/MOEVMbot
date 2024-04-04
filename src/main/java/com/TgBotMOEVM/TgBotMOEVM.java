b6872176dbdaa4037d364cdacf363b86e86ff869
Lyubava0382 <lubovkrivencova@gmail.com>
2024-04-05 00:44:23 +0300

    Demo bot version 1

---
+++ src/main/java/com/TgBotMOEVM/TgBotMOEVM.java
@@ -0,0 +1,13 @@
+package com.TgBotMOEVM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotMOEVM {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBotMOEVM.class, args);
+	}
+
+}
